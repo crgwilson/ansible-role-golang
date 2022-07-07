3646f85efe0d1172d36f5d41a9676840d0d0a34b
Craig Wilson <craigjwilson93@gmail.com>
2022-07-06 22:38:22 -0700

    Update with newer go version

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,16 +1,8 @@
-import os
-
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
-
 def test_default_go(host):
     f = host.file('/usr/local/bin/go')
     assert f.is_symlink
 
 
 def test_default_go_version(host):
-    go_version = 'go version go1.14.4 linux/amd64'
+    go_version = 'go version go1.18.3 linux/amd64'
     assert host.check_output('/usr/local/bin/go version') == go_version
